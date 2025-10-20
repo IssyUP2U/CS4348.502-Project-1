d1c0577a0c06b726a835b7485b23ce526a225bc0
Isabella Sobey <111943714+IssyUP2U@users.noreply.github.com>
2025-10-20 14:08:49 -0500

    Basic Encryption
    Accepts key and password then encrypts and decrypts it. Needs to be tweaked to not encrypt into non-letter symbols.

---
+++ encrypt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#define STR_SIZE 11
+
+int main() {
+    char key[STR_SIZE], pass[STR_SIZE];
+
+    printf("Enter a key (10 letters long): ");
+    fgets(key, 10, stdin);
+    while (getchar() != '\n'); 
+    key[STR_SIZE-1] = '\0';
+    
+    printf("\nEnter your password (10 letters max): ");
+    scanf("%10s", pass);
+    pass[STR_SIZE-1] = '\0';
+    while (getchar() != '\n'); 
+
+    printf("Key: %s\nPassword: %s\n", key, pass);
+
+    char encrypted_pass[STR_SIZE];
+    char decrypted_pass[STR_SIZE];
+    for (int i = 0; i < STR_SIZE; i++){
+        encrypted_pass[i] = pass[i] + key[i];
+    }
+    encrypted_pass[STR_SIZE-1] = '\0';
+    for (int i = 0; i < STR_SIZE; i++){
+        decrypted_pass[i] = encrypted_pass[i] - key[i];
+    }
+    decrypted_pass[STR_SIZE-1] = '\0';
+
+    printf("\nEncryption: %s\nDecryption: %s\n", encrypted_pass, decrypted_pass);
+}
